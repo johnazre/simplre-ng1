b3d19c0581f6e6d4e4ca363834572febc9b5fb48
johnazre <jaboyak@gmail.com>
2015-12-07 12:27:51 -0600

    dropdown menu added tadded to inventory page

--- Public/Controllers/MainCtrl.js
+++ Public/Controllers/MainCtrl.js
@@ -1,4 +1,4 @@
-angular.module('simplreApp').controller('MainCtrl', function ($scope, MainSvc) {
+angular.module('simplreApp').controller('MainCtrl', function ($scope, MainSvc, $mdDialog, $mdMedia) {
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
@@ -28,20 +28,77 @@ angular.module('simplreApp').controller('MainCtrl', function ($scope, MainSvc) {
       icon: 'settings'
     }
   ];
+
+  $scope.messages = [{
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }];
+
+  var originatorEv;
+    $scope.openMenu = function($mdOpenMenu, ev) {
+      originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
+
+  $scope.status = '  ';
+  $scope.customFullscreen = $mdMedia('sm');
+  $scope.showAlert = function(ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title('This is an alert title')
+        .textContent('You can specify some description text in here.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Got it!')
+        .targetEvent(ev)
+    );
+  }
+
+
 });
-  app.config(function($mdThemingProvider) {
-    var customBlueMap = 		$mdThemingProvider.extendPalette('light-blue', {
-      'contrastDefaultColor': 'light',
-      'contrastDarkColors': ['50'],
-      '50': 'ffffff'
-    });
-    $mdThemingProvider.definePalette('customBlue', customBlueMap);
-    $mdThemingProvider.theme('default')
-      .primaryPalette('customBlue', {
-        'default': '500',
-        'hue-1': '50'
-      })
-      .accentPalette('pink');
-    $mdThemingProvider.theme('input', 'default')
-          .primaryPalette('grey')
+
+
+
+
+app.config(function($mdThemingProvider) {
+  var customBlueMap = 		$mdThemingProvider.extendPalette('light-blue', {
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': ['50'],
+    '50': 'ffffff'
   });
+  $mdThemingProvider.definePalette('customBlue', customBlueMap);
+  $mdThemingProvider.theme('default')
+    .primaryPalette('customBlue', {
+      'default': '500',
+      'hue-1': '50'
+    })
+    .accentPalette('pink');
+  $mdThemingProvider.theme('input', 'default')
+        .primaryPalette('grey')
+});
